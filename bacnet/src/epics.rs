addf05602331e9028d899bb46bf7d3497c576810
Thomas Jespersen <tj@omnio.net>
2022-01-11 15:26:23 +0100

    bacnet: Remove PROFILE_PROPERTIES from epics

--- bacnet/src/epics.rs
+++ bacnet/src/epics.rs
@@ -22,25 +22,3 @@ pub struct Object {
     present_value: (), // Option<BACnetValue>,
     unit: String,
 }
-
-// List of properties to fetch for a profile
-pub(crate) const PROFILE_PROPERTIES: [bacnet_sys::BACNET_PROPERTY_ID; 7] = [
-    bacnet_sys::BACNET_PROPERTY_ID_PROP_OBJECT_NAME,
-    bacnet_sys::BACNET_PROPERTY_ID_PROP_OBJECT_TYPE,
-    bacnet_sys::BACNET_PROPERTY_ID_PROP_PRESENT_VALUE,
-    bacnet_sys::BACNET_PROPERTY_ID_PROP_STATUS_FLAGS,
-    bacnet_sys::BACNET_PROPERTY_ID_PROP_EVENT_STATE,
-    bacnet_sys::BACNET_PROPERTY_ID_PROP_OUT_OF_SERVICE,
-    bacnet_sys::BACNET_PROPERTY_ID_PROP_UNITS,
-];
-
-// Don't forget these:
-//
-//  {
-//    ...
-//    priority-array: { Null,Null,Null,Null,Null,Null,Null,Null,Null,Null,Null,Null,Null,Null,Null,Null }
-//    relinquish-default: 0.000000
-//    current-command-priority:     -- unknown property
-//  },
-//
-//
